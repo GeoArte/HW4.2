9f0ddb0f738eed6c812bddfa9873542465c82e43
gosha <gosha.artemov@gmail.com>
2023-09-17 10:08:25 +0400

    I have a problem

--- src/test/java/com/skypro/springintegrationtesting/user/UserControllerIntegrationTest.java
+++ src/test/java/com/skypro/springintegrationtesting/user/UserControllerIntegrationTest.java
@@ -1,1 +1,80 @@
+package com.skypro.springintegrationtesting.user;
 
+
+import com.skypro.springintegrationtesting.model.User;
+import com.skypro.springintegrationtesting.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class UserControllerIntegrationTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        userRepository.deleteAll();
+        userRepository.save(new User(1, "User1"));
+        userRepository.save(new User(2, "User2"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testGetAllUsers() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2));
+    }
+
+    @Test
+    @WithMockUser
+    public void testAddUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"User3\"}"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("User3"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testEditUser() throws Exception {
+        User user = userRepository.save(new User(null, "UserToEdit"));
+        mockMvc.perform(MockMvcRequestBuilders.put("/user/" + user.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"EditedUser\"}"))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(user.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("EditedUser"));
+    }
+
+    @Test
+    @WithMockUser
+    public void testDeleteUser() throws Exception {
+        User user = userRepository.save(new User(4, "UserToDelete"));
+        mockMvc.perform(MockMvcRequestBuilders.delete("/user/" + user.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+}
